91116b21ddc1c305279ddf9e0ef237a2123ff28e
Rejin Nepal <rejinnepal815@gmail.com>
2024-09-14 22:46:48 -0500

    Integrated AirportDB API to the project to search the airport based on ICAO code and integrated Google Maps API to show the location of the airport

--- src/backend/airport.service.ts
+++ src/backend/airport.service.ts
@@ -1,23 +1,23 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AirportService {
+@Injectable({
+  providedIn: 'root'
+})
+export class AirportService {
 
-//   private apiUrl = 'https://airportdb.io/api/v1/airport'; // Replace with your actual API URL
-//   private apiToken = 'e038b657c6ca2cf3491b83702995b10b8d32c5dd8175464e1925fdaef7f3bf16a5e7e26a69a3f9c9d95323eb95149558';
+  private apiUrl = 'https://airportdb.io/api/v1/airport';
+  private apiToken = 'e038b657c6ca2cf3491b83702995b10b8d32c5dd8175464e1925fdaef7f3bf16a5e7e26a69a3f9c9d95323eb95149558';
 
-//   constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-//   getAirports(): Observable<any> {
-//     return this.http.get(`${this.apiUrl}/airports`);
-//   }
+  getAirports(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/airports`);
+  }
 
-//   getAirportByCode(code: string): Observable<any> {
-//     const url = `${this.apiUrl}/${code}?apiToken=${this.apiToken}`;
-//     return this.http.get(url);
-//   }
-// }
+  getAirportByCode(code: string): Observable<any> {
+    const url = `${this.apiUrl}/${code}?apiToken=${this.apiToken}`;
+    return this.http.get(url);
+  }
+}
